4ff0ca7972591d7b58d62ecefe6e2cfee29f5c29
Nick Carneiro <burthawk101@gmail.com>
2012-09-25 00:50:38 -0500

    remove all spaces and rename variables. 250 bytes

--- bintoascii.js
+++ bintoascii.js
@@ -1,7 +1,6 @@
-bin = '0110011001110101011000110110101100100000011011010110100101100011011100100110111101100011011011110110010001100101'
-		output = ''
-		for (i = 0; i < bin.length; i += 8) {
-			substr = bin.substr(i, 8)
-			output += String.fromCharCode(parseInt(substr, 2));                    
-		}
-console.log(output)
+bin='0110011001110101011000110110101100100000011011010110100101100011011100100110111101100011011011110110010001100101'
+o=''
+for(i=0;i<bin.length;i+=8){
+substr=bin.substr(i, 8)
+o+=String.fromCharCode(parseInt(substr, 2));                    
+}console.log(o)
